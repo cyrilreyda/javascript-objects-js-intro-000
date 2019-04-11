fb087e2a1906c72e340129809b8c1fd2ad448218
Cyril Reida <creyda@gmail.com>
2019-04-11 19:02:27 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,10 @@
 let playlist = {
+  Doors: "People are strange",
+  Beatles: "Yesterday",
+  Rolling: "Not today"
 }
 
 const updatePlaylist = (playlist, artist, songTitle) => {
-  playlist.artist = songTitle;
+  playlist[artist] = songTitle;
   return playlist;
 }
